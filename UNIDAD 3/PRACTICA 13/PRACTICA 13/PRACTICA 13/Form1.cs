e33d8c39d7d83501c94f8d9624e88ce9d15ea44d
LUISRODRIGUEZ0408 <rdzluisrdz17@gmail.com>
2024-11-15 12:52:51 -0600

    Add files via upload

---
+++ UNIDAD 3/PRACTICA 13/PRACTICA 13/PRACTICA 13/Form1.cs
@@ -0,0 +1,30 @@
+using System.Net;
+
+namespace PRACTICA_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sueldo, aum, nsue;
+            sueldo = double.Parse(textBox1.Text);
+            if (sueldo < 400000.00)
+            {
+                aum = sueldo * 0.15;
+                nsue = sueldo + aum;
+                textBox2.Text = nsue.ToString();
+            }
+
+        }
+
+        private void aum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
